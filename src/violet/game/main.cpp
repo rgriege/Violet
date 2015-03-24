11e375116bcd6899b7c1b61471a78ca08acd276b
Ryan Griege <rgriege@sbcglobal.net>
2015-03-24 00:05:24 -0500

    Condensed application code

--- src/violet/game/main.cpp
+++ src/violet/game/main.cpp
@@ -1,12 +1,37 @@
 #include "violet/core/Engine.h"
+#include "violet/core/script/CppScript.h"
+#include "violet/core/script/system/ScriptSystem.h"
 #include "violet/core/serialization/FileDeserializerFactory.h"
-#include "Setup.h"
+#include "violet/core/system/SystemFactory.h"
+#include "violet/core/transform/TransformSystem.h"
+#include "violet/extras/serialization/JsonDeserializer.h"
+#include "violet/plugins/graphics/system/RenderSystem.h"
+#include "violet/plugins/physics/system/PhysicsSystem.h"
+#include "violet/plugins/input/system/InputSystem.h"
+#include "violet/plugins/update/system/UpdateSystem.h"
 
 #include <iostream>
 
-int main(int argc, char** argv)
+SystemFactory setup()
 {
-	auto factory = setup(argc, argv);
+	Violet::JsonDeserializer::install();
+
+	Violet::CppScript::install();
+
+	Violet::SystemFactory factory;
+	Violet::TransformSystem::install(factory);
+	Violet::PhysicsSystem::install(factory);
+	Violet::RenderSystem::install(factory);
+	Violet::InputSystem::install(factory);
+	Violet::ScriptSystem::install(factory);
+	Violet::UpdateSystem::install(factory);
+
+	return factory;
+}
+
+int main(int /*argc*/, char ** /*argv*/)
+{
+	auto factory = setup();
 
 	auto deserializer = FileDeserializerFactory::getInstance().create("config.json");
 	if (deserializer == nullptr || !*deserializer)
