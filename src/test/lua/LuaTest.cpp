caec858573559bdfde77bb6cc888fac59fa48569
Ryan <rgriege@sbcglobal.net>
2015-08-20 22:45:50 -0500

    Adds more lua tests. Registered c functions with parameters and return value.

--- src/test/lua/LuaTest.cpp
+++ src/test/lua/LuaTest.cpp
@@ -19,15 +19,24 @@ namespace LuaTestNamespace
 		"function init()\n"
 		"	sayHiInC()\n"
 		"end\n"
-		"function getFortyTwo()\n"
+		"function get42()\n"
 		"	return 42\n"
 		"end\n"
-		"function addFive(x)\n"
+        "function get42FromC()\n"
+        "   return get42InC()\n"
+        "end\n"
+		"function add5(x)\n"
 		"	return x + 5\n"
+		"end\n"
+		"function add5FromC(x)\n"
+		"	return add5InC(x)\n"
 		"end\n"
 		"function sum(x, y)\n"
 		"	return x + y\n"
 		"end\n"
+        "function getSumFromC(x, y)\n"
+        "   return addInC(x, y)\n"
+        "end\n"
 		"function clean() end\n");
 
 	shared_val<TextResource> m_invalidScriptSource = make_shared_val<StringResource>("invalid.inl",
@@ -45,7 +54,9 @@ namespace LuaTestNamespace
 	// ----------------------------------------------------------------------------
 
 	void sayHiFromLua();
-	int divideFromLua(int x, int y);
+    int get42();
+	int add5(int x);
+	int add(int x, int y);
 
 	// ----------------------------------------------------------------------------
 }
@@ -57,6 +68,9 @@ using namespace LuaTestNamespace;
 void LuaTest::run(TestEvaluator & evaluator)
 {
 	Violet::LuaLibrary::addMethod(Violet::make_unique_val<Violet::LuaMethod<void(void)>>("sayHiInC", sayHiFromLua));
+	Violet::LuaLibrary::addMethod(Violet::make_unique_val<Violet::LuaMethod<int(void)>>("get42InC", get42));
+	Violet::LuaLibrary::addMethod(Violet::make_unique_val<Violet::LuaMethod<int(int)>>("add5InC", add5));
+	Violet::LuaLibrary::addMethod(Violet::make_unique_val<Violet::LuaMethod<int(int, int)>>("addInC", add));
 
 	TestFactory::makeStatelessSuite("lua tests", std::forward_as_tuple(
 		TestFactory::makeStatelessSuite("load tests", std::forward_as_tuple(
@@ -65,12 +79,15 @@ void LuaTest::run(TestEvaluator & evaluator)
 			TestFactory::makeStateless("no init", false, []() { return LuaScript(m_noInitScriptSource).isValid(); })
 		)),
 		TestFactory::makeStatelessSuite("lua function calls", std::forward_as_tuple(
-			TestFactory::makeStateless("int(void)", 42, []() { return LuaScript(m_validScriptSource).run<int>("getFortyTwo"); }),
-			TestFactory::makeStateless("int(int)", 42, []() { return LuaScript(m_validScriptSource).run<int>("addFive", 37); }),
+			TestFactory::makeStateless("int(void)", 42, []() { return LuaScript(m_validScriptSource).run<int>("get42"); }),
+			TestFactory::makeStateless("int(int)", 42, []() { return LuaScript(m_validScriptSource).run<int>("add5", 37); }),
 			TestFactory::makeStateless("int(int, int)", 42, []() { return LuaScript(m_validScriptSource).run<int>("sum", 14, 28); })
 		)),
 		TestFactory::makeStatelessSuite("c function calls", std::forward_as_tuple(
-			TestFactory::makeStateless("void(void)", true, []() { return saidHi; })
+			TestFactory::makeStateless("void(void)", true, []() { return saidHi; }),
+			TestFactory::makeStateless("int(void)", 42, []() { return LuaScript(m_validScriptSource).run<int>("get42FromC"); }),
+			TestFactory::makeStateless("int(int)", 42, []() { return LuaScript(m_validScriptSource).run<int>("add5FromC", 37); }),
+			TestFactory::makeStateless("int(int, int)", 42, []() { return LuaScript(m_validScriptSource).run<int>("getSumFromC", 14, 28); })
 		))
 	)).evaluate(evaluator);
 }
@@ -84,9 +101,23 @@ void LuaTestNamespace::sayHiFromLua()
 
 // ----------------------------------------------------------------------------
 
-int LuaTestNamespace::divideFromLua(const int x, const int y)
+int LuaTestNamespace::get42()
 {
-	return x / y;
+    return 42;
+}
+
+// ----------------------------------------------------------------------------
+
+int LuaTestNamespace::add5(const int x)
+{
+	return x + 5;
+}
+
+// ----------------------------------------------------------------------------
+
+int LuaTestNamespace::add(const int x, const int y)
+{
+	return x + y;
 }
 
 // ============================================================================
