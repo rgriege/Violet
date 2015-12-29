fc9e311751d5f2396d9ea71dd07f1efd19e521c4
Ryan <rgriege@sbcglobal.net>
2015-12-29 10:22:59 -0600

    Removed data cruft

--- res/entityListElementMethods.h
+++
@@ -1,7 +0,0 @@
-#include "violet/Engine.h"
-
-class Violet::Engine;
-
-DEFINE_EXTERNAL_METHOD(DescribeMethod, void (const Violet::Engine & engine));
-DEFINE_EXTERNAL_METHOD(ExpandMethod, void ());
-DEFINE_EXTERNAL_METHOD(ShrinkMethod, void ());
