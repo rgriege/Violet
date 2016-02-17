0434cf2524a54469dd3fd6366e120c4b739a0f39
Ryan Griege <rgriege@sbcglobal.net>
2016-02-17 09:31:39 -0600

    Editor command to toggle entity labels

--- res/editorSceneManager.cpp
+++ res/editorSceneManager.cpp
@@ -1,12 +1,15 @@
-#include "editor/editor_system.h"
-#include "editor/cpp_script_exports.h"
 #include "editor/command/file/open_command.h"
 #include "editor/command/file/save_all_command.h"
+#include "editor/component/editor_component.h"
+#include "editor/cpp_script_exports.h"
+#include "editor/editor_system.h"
 #include "violet/core/engine.h"
 #include "violet/component/scene.h"
+#include "violet/graphics/component/text_component.h"
 #include "violet/input/system/input_system.h"
 #include "violet/log/log.h"
 #include "violet/script/cpp/cpp_script.h"
+#include "violet/transform/component/local_transform_component.h"
 #include "violet/utility/formatted_string.h"
 #include "violet/utility/memory.h"
 
@@ -15,7 +18,25 @@
 using namespace edt;
 using namespace vlt;
 
-/*static void show_labels_task(void * mem)
+struct assign_label_task_data
+{
+	handle label_id;
+	handle proxy_id;
+};
+
+static void assign_label_task(void * mem)
+{
+	auto data = make_unique<assign_label_task_data>(mem);
+	auto * tc = engine::instance().get_current_scene().get_component<text_component>(data->label_id);
+	if (tc)
+		tc->m_text = formatted_string<16>().sprintf("%d", data->proxy_id.id);
+
+	auto * ltc = engine::instance().get_current_scene().get_component<local_transform_component>(data->label_id);
+	if (ltc)
+		ltc->parent_id = data->proxy_id;
+}
+
+static void show_labels_task(void * mem)
 {
 	auto label_entities = static_cast<std::vector<handle>*>(mem);
 	auto & scene = engine::instance().get_current_scene();
@@ -24,11 +45,20 @@ using namespace vlt;
 	{
 		if (scene.has_component<editor_component>(entity_id))
 		{
-			label_entities->emplace_back(scene.load("editor_label.json"));
-			add_task(assign_label_task, new assign_label_task_data{ label_entities, entity_id.id }, 0, task_type::write);
+			label_entities->emplace_back(scene.load("editor_label.json").front());
+			add_task(assign_label_task, new assign_label_task_data{ label_entities->back(), entity_id }, 0, task_type::write);
 		}
 	}
-}*/
+}
+
+static void hide_labels_task(void * mem)
+{
+	auto label_entities = static_cast<std::vector<handle>*>(mem);
+	auto & scene = engine::instance().get_current_scene();
+	for (const handle entity_id : *label_entities)
+		scene.remove_all(entity_id);
+	label_entities->clear();
+}
 
 static void load_dialog_task(void * mem)
 {
@@ -48,8 +78,8 @@ struct instance final : public cpp_script::instance
 {
 	instance(cpp_script & script) :
 		cpp_script::instance(script),
-		dialog_type(None),
-		dialog_entities()
+		dialog_entities(),
+		label_entities()
 	{
 		KeyUpMethod::assign(script, KeyUpMethod::Handler::bind<instance, &instance::on_key_up>(this));
 		DialogClosedEvent::subscribe(engine::instance(), DialogClosedEvent::subscriber::bind<instance, &instance::onDialogClosed>(this));
@@ -73,6 +103,13 @@ struct instance final : public cpp_script::instance
 			}
 			break;
 
+		case 't':
+			if (label_entities.empty())
+				add_task(show_labels_task, &label_entities, 0, task_type::write);
+			else
+				add_task(hide_labels_task, &label_entities, 0, task_type::write);
+			break;
+
 		case 'z':
 			if ((event.modifiers & key::modifier::M_CTRL) != 0)
 			{
@@ -83,7 +120,6 @@ struct instance final : public cpp_script::instance
 		case ';':
 			if (event.modifiers & key::modifier::M_SHIFT)
 			{
-				// add_task(show_labels_task, &label_entites, 0, task_type::write);
 				add_task(load_dialog_task, &dialog_entities, 0, task_type::write);
 			}
 			break;
