d573164e226df7b58f55f0ff8674632cd5a62ac5
Ryan Griege <rgriege@sbcglobal.net>
2016-02-16 23:59:29 -0600

    Fixed up move command

--- src/editor/command/move_to_command.cpp
+++ src/editor/command/move_to_command.cpp
@@ -2,6 +2,7 @@
 
 #include "editor/command/move_to_command.h"
 
+#include "editor/component/editor_component.h"
 #include "editor/editor_system.h"
 #include "violet/core/engine.h"
 #include "violet/component/scene.h"
@@ -43,9 +44,9 @@ std::unique_ptr<command> move_to_command::parse(const std::string & text)
 
 // ============================================================================
 
-move_to_command::move_to_command(const handle entity_id, const v2 & position) :
-	m_entityId(entity_id),
-	m_position(position)
+move_to_command::move_to_command(const handle _entity_id, const v2 & _position) :
+	entity_id(_entity_id),
+	position(_position)
 {
 }
 
@@ -53,63 +54,56 @@ move_to_command::move_to_command(const handle entity_id, const v2 & position) :
 
 struct move_to_command_execute_task_data
 {
-	handle entity_id;
+	handle proxy_id;
 	v2 position;
 };
 
 static void execute_task_local(void * mem)
 {
 	auto data = make_unique<move_to_command_execute_task_data>(mem);
-	auto & editor = *engine::instance().get_system<editor_system>();
-	auto ltc = editor.get_scene().get_component<local_transform_component>(data->entity_id);
-	if (ltc != nullptr)
-	{
-		transform::set_position(ltc->transform, data->position);
-		const handle proxy_id = editor.get_proxy_id(data->entity_id);
-		engine::instance().get_current_scene().get_component<local_transform_component>(proxy_id)->transform = ltc->transform;
-	}
+	auto & proxy_scene = engine::instance().get_current_scene();
+	transform::set_position(proxy_scene.get_component<local_transform_component>(data->proxy_id)->transform, data->position);
+
+	// todo: async (can't ready editor_component here)
+	const handle proxied_id = proxy_scene.get_component<editor_component>(data->proxy_id)->proxied_id;
+	auto * proxied_ltc = engine::instance().get_system<editor_system>()->get_scene().get_component<local_transform_component>(proxied_id);
+	transform::set_position(proxied_ltc->transform, data->position);
 }
 
 static void execute_task_world(void * mem)
 {
 	auto data = make_unique<move_to_command_execute_task_data>(mem);
-	auto & editor = *engine::instance().get_system<editor_system>();
-	auto wtc = editor.get_scene().get_component<world_transform_component>(data->entity_id);
-	if (wtc != nullptr)
-	{
-		transform::set_position(wtc->transform, data->position);
-		// const handle proxy_id = editor.get_proxy_id(data->entity_id);
-		// engine::instance().get_current_scene().get_component<world_transform_component>(proxy_id)->transform = ltc->transform;
-	}
+	auto & proxy_scene = engine::instance().get_current_scene();
+	transform::set_position(proxy_scene.get_component<world_transform_component>(data->proxy_id)->transform, data->position);
+
+	// todo: async (can't ready editor_component here)
+	const handle proxied_id = proxy_scene.get_component<editor_component>(data->proxy_id)->proxied_id;
+	auto * proxied_wtc = engine::instance().get_system<editor_system>()->get_scene().get_component<world_transform_component>(proxied_id);
+	transform::set_position(proxied_wtc->transform, data->position);
 }
 
 void move_to_command::execute()
 {
-	log(formatted_string<128>().sprintf("MoveTo::execute %d", m_entityId.id));
-	const auto & engine = engine::instance();
-	const auto & editor = *engine.get_system<editor_system>();
-	const auto & scene = editor.get_scene();
-	if (m_entityId.version == handle::Invalid.version)
-		m_entityId = handle(m_entityId.id, scene.get_entity_version(m_entityId.id));
+	const auto & proxy_scene = engine::instance().get_current_scene();
+	entity_id = handle(entity_id.id, proxy_scene.get_entity_version(entity_id.id));
+	log(formatted_string<128>().sprintf("MoveTo::execute %d", entity_id.id));
 
-	const handle entity_id = m_entityId;
-	const v2 newPosition = m_position;
-
-	const auto * ltc = scene.get_component<local_transform_component>(m_entityId);
+	const auto * ltc = proxy_scene.get_component<local_transform_component>(entity_id);
 	if (ltc != nullptr)
 	{
-		log(formatted_string<128>().sprintf("MoveTo local <%f,%f>", m_position.x, m_position.y));
-		add_task(execute_task_local, new move_to_command_execute_task_data{ entity_id, m_position }, local_transform_component::metadata->thread, task_type::write);
-		m_position = transform::get_position(ltc->transform);
+		log(formatted_string<128>().sprintf("MoveTo local <%f,%f>", position.x, position.y));
+		add_task(execute_task_local, new move_to_command_execute_task_data{ entity_id, position }, local_transform_component::metadata->thread, task_type::write);
+		position = transform::get_position(ltc->transform);
+		return;
 	}
-	else
+	
+	const auto * wtc = proxy_scene.get_component<world_transform_component>(entity_id);
+	if (wtc != nullptr)
 	{
-		const auto * wtc = scene.get_component<world_transform_component>(m_entityId);
-		if (wtc != nullptr)
-		{
-			log(formatted_string<128>().sprintf("MoveTo world <%f,%f>", m_position.x, m_position.y));
-			m_position = transform::get_position(wtc->transform);
-		}
+		log(formatted_string<128>().sprintf("MoveTo world <%f,%f>", position.x, position.y));
+		add_task(execute_task_world, new move_to_command_execute_task_data{ entity_id, position }, world_transform_component::metadata->thread, task_type::write);
+		position = transform::get_position(wtc->transform);
+		return;
 	}
 }
 
--- src/editor/command/move_to_command.h
+++ src/editor/command/move_to_command.h
@@ -11,12 +11,11 @@ namespace edt
 {
 	struct EDITOR_API move_to_command final : public command
 	{
-	public:
+		vlt::handle entity_id;
+		vlt::v2 position;
 
 		static const char * get_usage();
 		static std::unique_ptr<command> parse(const std::string & text);
-
-	public:
 
 		move_to_command(vlt::handle entity_id, const vlt::v2 & position);
 		virtual ~move_to_command() override = default;
@@ -24,11 +23,6 @@ namespace edt
 		virtual void execute() override;
 		virtual bool can_undo() const override;
 		virtual void undo() override;
-
-	private:
-
-		vlt::handle m_entityId;
-		vlt::v2 m_position;
 	};
 }
 
