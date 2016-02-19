54b27b9c760c0941394741a7093c183b2efd8e78
Ryan <rgriege@sbcglobal.net>
2016-02-18 21:48:45 -0600

    Fixed MSVC file rename

--- src/violet/math/interval.cpp
+++ src/violet/math/interval.cpp
@@ -1,88 +1,32 @@
-// ============================================================================
+#ifndef VIOLET_INTERVAL_H
+#define VIOLET_INTERVAL_H
 
-#include <algorithm>
-#include <iostream>
-#include <cmath>
+#include <iosfwd>
 
-#include "violet/math/interval.h"
+#include "violet/core/defines.h"
+#include "violet/core/types.h"
 
-using namespace vlt;
-
-// ============================================================================
-
-interval::interval() :
-	left(0),
-	right(0)
+namespace vlt
 {
+	struct VIOLET_API interval
+	{
+		r32 left, right;
+
+		interval();
+		interval(r32 left, r32 right);
+		interval(const interval & other);
+
+		interval & slide(r32 delta);
+
+		r32 length() const;
+		
+		bool contains(r32 x) const;
+		bool contains(const interval & other) const;
+		bool overlaps(const interval & other) const;
+		r32 overlap(const interval & other) const;
+	};
+
+	VIOLET_API std::ostream & operator<<(std::ostream & os, const interval & interval);
 }
 
-// ----------------------------------------------------------------------------
-
-interval::interval(const r32 left, const r32 right) :
-	left(left),
-	right(right)
-{
-}
-
-// ----------------------------------------------------------------------------
-
-interval::interval(const interval & other) :
-	left(other.left),
-	right(other.right)
-{
-}
-
-// ----------------------------------------------------------------------------
-
-interval & interval::slide(const r32 delta)
-{
-	left += delta;
-	right += delta;
-	return *this;
-}
-
-// ----------------------------------------------------------------------------
-
-r32 interval::length() const
-{
-	return right - left;
-}
-
-// ----------------------------------------------------------------------------
-
-bool interval::contains(const r32 x) const
-{
-	return x >= left && x <= right;
-}
-
-// ----------------------------------------------------------------------------
-
-bool interval::contains(const interval & other) const
-{
-	return contains(other.left) && contains(other.right);
-}
-
-// ----------------------------------------------------------------------------
-
-bool interval::overlaps(const interval & other) const
-{
-	return contains(other.left) || other.contains(left);
-}
-
-// ----------------------------------------------------------------------------
-
-r32 interval::overlap(const interval &  other) const
-{
-	r32 diff1 = right - other.left;
-	r32 diff2 = other.right - left;
-	return diff1 < 0 || diff2 < 0 ? 0 : std::min(std::min(diff1, diff2), std::min(length(), other.length()));
-}
-
-// ============================================================================
-
-std::ostream & vlt::operator<<(std::ostream & os, const interval & interval)
-{
-	return os << "[" << interval.left << "," << interval.right << "]";
-}
-
-// ============================================================================
+#endif
