bfc91544b41b1333d62f690632f1e213c9616555
Ryan Griege <rgriege@sbcglobal.net>
2016-06-13 23:15:50 -0500

    improved time diff

--- utility/time.c
+++ utility/time.c
@@ -1,4 +1,32 @@
+#include <assert.h>
+#include <time.h>
+
 #include "violet/utility/time.h"
+
+static b8 timespec_diff(const struct timespec * start, const struct timespec * stop,
+                        struct timespec * res)
+{
+	struct timespec mod_start;
+	mod_start.tv_nsec = start->tv_nsec;
+	mod_start.tv_sec = start->tv_sec;
+
+	if (stop->tv_nsec < mod_start.tv_nsec)
+	{
+		mod_start.tv_nsec -= 1000000000;
+		++mod_start.tv_sec;
+	}
+	/* C standard requires states that tv_nsec should be [0, 999,999,999] */
+	assert(stop->tv_nsec - mod_start.tv_nsec < 1000000000);
+
+	if (stop->tv_sec >= mod_start.tv_sec)
+	{
+		res->tv_sec = stop->tv_sec - mod_start.tv_sec;
+		res->tv_nsec = stop->tv_nsec - mod_start.tv_nsec;
+		return true;
+	}
+	else
+		return false;
+}
 
 #ifdef __unix__
 
@@ -9,6 +37,7 @@ void vlt_get_time(vlt_time * t)
 
 u32 vlt_diff_milli(vlt_time * start, vlt_time * end)
 {
+	// TODO(rgriege): handle start > end (see timespec_diff)
 	return (end->tv_sec * 1000 + end->tv_nsec / 1000000) -
 		(start->tv_sec * 1000 + start->tv_nsec / 1000000);
 }
