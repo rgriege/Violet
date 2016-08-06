c74f3e5dac59506c3315696a050267830af5d766
rgriege <rgriege@sbcglobal.net>
2016-08-06 13:55:40 -0500

    array_map modernization pass

--- structures/array_map.c
+++ structures/array_map.c
@@ -4,20 +4,20 @@
 
 #include "violet/structures/array_map.h"
 
-void array_map_init(array_map * m, u32 key_size, u32 val_size)
+void array_map_init(array_map *m, u32 key_size, u32 val_size)
 {
 	array_init(&m->pairs, key_size + val_size);
 	m->key_size = key_size;
 }
 
-void array_map_copy(array_map * dest, const array_map * src)
+void array_map_copy(array_map *dest, const array_map *src)
 {
 	array_copy(&dest->pairs, &src->pairs);
 	dest->key_size = src->key_size;
 }
 
-void array_map_copy_each(array_map * dest,
-                         const array_map * src,
+void array_map_copy_each(array_map *dest,
+                         const array_map *src,
                          void(*copy_key)(void*,const void*),
                          void(*copy_val)(void*,const void*))
 {
@@ -38,12 +38,12 @@ void array_map_copy_each(array_map * dest,
 	}
 }
 
-void array_map_destroy(array_map * m)
+void array_map_destroy(array_map *m)
 {
 	array_destroy(&m->pairs);
 }
 
-void array_map_destroy_each_ex(array_map * m,
+void array_map_destroy_each_ex(array_map *m,
                                void(*destroy_key)(void*),
                                void(*destroy_val)(void*))
 {
@@ -58,35 +58,35 @@ void array_map_destroy_each_ex(array_map * m,
 	array_map_destroy(m);
 }
 
-void array_map_insert(array_map * m, const void * key, const void * val)
+void array_map_insert(array_map *m, const void *key, const void *val)
 {
-	void * p = array_map_insert_null(m, key);
+	void *p = array_map_insert_null(m, key);
 	memcpy(p, val, m->pairs.elem_size - m->key_size);
 }
 
-void * array_map_insert_null(array_map * m, const void * key)
+void *array_map_insert_null(array_map *m, const void *key)
 {
 	assert(!array_map_has(m, key));
-	void * p = array_append_null(&m->pairs);
+	void *p = array_append_null(&m->pairs);
 	memcpy(p, key, m->key_size);
 	return (byte*)p + m->key_size;
 }
 
-u32 array_map_size(const array_map * m)
+u32 array_map_size(const array_map *m)
 {
 	return array_size(&m->pairs);
 }
 
-u32 array_map_val_size(const array_map * m)
+u32 array_map_val_size(const array_map *m)
 {
 	return m->pairs.elem_size - m->key_size;
 }
 
-void * array_map_get(const array_map * m, const void * key)
+void *array_map_get(const array_map *m, const void *key)
 {
 	for (u32 i = 0, n = array_map_size(m); i < n; ++i)
 	{
-		void * elem = array_get(&m->pairs, i);
+		void *elem = array_get(&m->pairs, i);
 		if (memcmp(key, elem, m->key_size) == 0)
 			return (byte*)elem + m->key_size;
 	}
@@ -94,12 +94,12 @@ void * array_map_get(const array_map * m, const void * key)
 	return NULL;
 }
 
-b8 array_map_has(const array_map * m, const void * key)
+b8 array_map_has(const array_map *m, const void *key)
 {
 	return array_map_get(m, key) != NULL;
 }
 
-b8 array_map_iterate(const array_map * m, array_map_iter * iter)
+b8 array_map_iterate(const array_map *m, array_map_iter *iter)
 {
 	if (iter->next_idx >= array_map_size(m))
 		return false;
@@ -110,14 +110,14 @@ b8 array_map_iterate(const array_map * m, array_map_iter * iter)
 	return true;
 }
 
-void array_map_remove_iter(array_map * m, array_map_iter * iter)
+void array_map_remove_iter(array_map *m, array_map_iter *iter)
 {
 	--iter->next_idx;
 	assert(iter->next_idx < array_map_size(m));
 	array_remove(&m->pairs, iter->next_idx);
 }
 
-void array_map_iter_reset(array_map_iter * iter)
+void array_map_iter_reset(array_map_iter *iter)
 {
 	memset(iter, 0, sizeof(array_map_iter));
 }
--- structures/array_map.h
+++ structures/array_map.h
@@ -9,34 +9,35 @@ typedef struct array_map
 	u32 key_size;
 } array_map;
 
-void array_map_init(array_map * m, u32 key_size, u32 val_size);
-void array_map_copy(array_map * dest, const array_map * src);
-void array_map_copy_each(array_map * dest,
-                         const array_map * src,
+void array_map_init(array_map *m, u32 key_size, u32 val_size);
+void array_map_copy(array_map *dest, const array_map *src);
+void array_map_copy_each(array_map *dest,
+                         const array_map *src,
                          void(*copy_key)(void*,const void*),
                          void(*copy_val)(void*,const void*));
-void array_map_destroy(array_map * m);
-void array_map_destroy_each_ex(array_map * m,
+void array_map_destroy(array_map *m);
+void array_map_destroy_each_ex(array_map *m,
                                void(*destroy_key)(void*),
                                void(*destroy_val)(void*));
-#define array_map_destroy_each(m,k,v) array_map_destroy_each_ex(m, (void(*)(void*))k, (void(*)(void*))v)
-void array_map_insert(array_map * m, const void * key, const void * val);
-void * array_map_insert_null(array_map * m, const void * key);
-u32 array_map_size(const array_map * m);
-u32 array_map_val_size(const array_map * m);
-void * array_map_get(const array_map * m, const void * key);
-b8 array_map_has(const array_map * m, const void * key);
+#define array_map_destroy_each(m,k,v) \
+	array_map_destroy_each_ex(m, (void(*)(void*))k, (void(*)(void*))v)
+void array_map_insert(array_map *m, const void *key, const void *val);
+void *array_map_insert_null(array_map *m, const void *key);
+u32 array_map_size(const array_map *m);
+u32 array_map_val_size(const array_map *m);
+void *array_map_get(const array_map *m, const void *key);
+b8 array_map_has(const array_map *m, const void *key);
 
 typedef struct array_map_iter
 {
-	void * key;
-	void * val;
+	void *key;
+	void *val;
 	u32 next_idx;
 } array_map_iter;
 
-b8 array_map_iterate(const array_map * m, array_map_iter * iter);
-void array_map_remove_iter(array_map * m, array_map_iter * iter);
-void array_map_iter_reset(array_map_iter * iter);
+b8 array_map_iterate(const array_map *m, array_map_iter *iter);
+void array_map_remove_iter(array_map *m, array_map_iter *iter);
+void array_map_iter_reset(array_map_iter *iter);
 
 #endif
 
