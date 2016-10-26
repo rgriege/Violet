4b49af15394ebb7510171925ea2a0332743994be
rgriege <rgriege@sbcglobal.net>
2016-10-26 15:00:55 -0500

    Hide arrays in typed pointers
    Modeled after stb_stretchy_buffer, the array capacity and size are
    stored in the 8 bytes before the start of the array.  This allows
    maintaining the type information of the array.
    
    Leaving the old array struct since updating all usages will take some
    time.

---
+++ structures/arr.c
@@ -0,0 +1,91 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "violet/math/common.h"
+#include "violet/structures/arr.h"
+
+void *_arr_create()
+{
+	u32 *a = malloc(2*sizeof(u32));
+	a += 2;
+	arr_cap(a) = arr_sz(a) = 0;
+	return a;
+}
+
+void *_arr_reserve(void *a, size_t nmemb, size_t sz)
+{
+	if (nmemb > arr_cap(a))
+	{
+		u32 *p = realloc(arr_raw(a), nmemb*sz+2*sizeof(u32));
+		arr_cap(p+2) = nmemb;
+		return p+2;
+	}
+	else
+		return a;
+}
+
+void *_arr_insert_null(void *_a, u32 idx, size_t sz)
+{
+	byte *a = _a;
+	assert(idx <= arr_sz(a));
+	for (byte *p = a+(arr_sz(a)-1)*sz, *end=a+idx*sz; p!=end; p-=sz)
+		memcpy(p, p-sz, sz);
+	return a+idx*sz;
+}
+
+void _arr_remove(void *_a, void *_e, size_t sz)
+{
+	byte *a = _a;
+	byte *e = _e;
+	assert(e >= a && e < a+arr_sz(a)*sz && (e-a)%sz == 0);
+	for (byte *p = e+sz, *end=a+arr_sz(a)*sz; p!=end; p+=sz)
+		memcpy(p-sz, p, sz);
+	--arr_sz(a);
+}
+
+void _arr_reverse(void *_a, size_t sz)
+{
+	byte *a = _a;
+	byte *scratch = malloc(sz);
+	for (u32 i = 0, n = arr_sz(a)/2; i < n; ++i)
+	{
+		byte *l = a+i*sz;
+		byte *r = a+(arr_sz(a)-1-i)*sz;
+		memcpy(scratch, r, sz);
+		memcpy(r, l, sz);
+		memcpy(l, scratch, sz);
+	}
+	free(scratch);
+}
+
+u32 _arr_contains(void *_a, const void *elem, size_t sz)
+{
+	byte *a = _a;
+	for (byte *p=a, *end=a+arr_sz(a)*sz; p!=end; p+=sz)
+		if (memcmp(elem, p, sz) == 0)
+			return (p-a)/sz;
+	return -1;
+}
+
+void *_arr_upper(void *_a, const void *key,
+                 int(*comp)(const void *, const void*), size_t sz)
+{
+	byte *a = _a;
+	u32 left = 0, right = arr_sz(a), mid = arr_sz(a)/2;
+	void *res = NULL;
+	while (left != right)
+	{
+		void *p = a+mid*sz;
+		if (comp(key, p) < 0)
+		{
+			res = p;
+			right = mid;
+		}
+		else
+			left = mid+1;
+		mid = (right+left)/2;
+	}
+	return res;
+}
+
---
+++ structures/arr.h
@@ -0,0 +1,60 @@
+#ifndef VIOLET_ARR_H
+#define VIOLET_ARR_H
+
+#include <stdlib.h>
+
+#include "violet/core/macros.h"
+#include "violet/core/types.h"
+#include "violet/math/common.h"
+#include "violet/utility/mem.h"
+
+// TODO(rgriege): replace array
+
+#define arr_raw(a)    (((u32*)(a))-2)
+#define arr_create()  _arr_create()
+#define arr_free(a)   free(arr_raw(a))
+
+#define arr_cap(a)     (arr_raw(a)[0])
+#define arr_sz(a)      (arr_raw(a)[1])
+#define arr_raw_sz(a)  (2*sizeof(u32)+arr_sz(a)*sizeof(*a))
+
+#define arr_cp(dst, src) \
+	arr_reserve(dst, arr_sz(src)); \
+	memcpy(arr_raw(dst)+1, arr_raw(src)+1, arr_sz(a)*sizeof(*a)+sizeof(u32))
+
+#define arr_last(a)               ((a)+arr_sz(a)-1)
+#define arr_end(a)                ((a)+arr_sz(a))
+#define arr_foreach(a, type, it)  for (type *it=a; it!=arr_end(a); ++it)
+
+#define arr_reserve(a, n) ((a)=_arr_reserve(a, n, sizeof(*a)))
+#define arr_grow(a) arr_reserve(a, max(arr_cap(a)*3/2, arr_cap(a)+1))
+#define arr_append_null(a) \
+	(arr_sz(a) == arr_cap(a) ? arr_grow(a) : 0, ++arr_sz(a), arr_last(a))
+#define arr_append(a, e) \
+	UNUSED(arr_append_null(a)), memcpy(arr_last(a), e, sizeof(*a))
+#define arr_insert_null(a, i) \
+	(UNUSED(arr_append_null(a)), _arr_insert_null(a, i, sizeof(*a)))
+#define arr_insert(a, i, e) memcpy(arr_insert_null(a, i), e, sizeof(*a))
+
+#define arr_remove(a, e)   _arr_remove(a, e, sizeof(*a))
+#define arr_pop(a)         arr_remove(a, arr_last(a))
+#define arr_clear(a)       arr_sz(a) = 0
+
+#define arr_reverse(a)          _arr_reverse(a, sizeof(*a))
+#define arr_qsort(a, cmp)       qsort(a, arr_sz(a), sizeof(*a), cmp)
+#define arr_bsearch(a, e, cmp)  bsearch(e, a, arr_sz(a), sizeof(*a), cmp)
+#define arr_contains(a, e)      (_arr_contains(a, e, sizeof(*a))!=-1)
+#define arr_index(a, e, i)      ((i=_arr_contains(a, e, sizeof(*a)))!=-1)
+#define arr_upper(a, e, cmp)    _arr_upper(a, e, cmp, sizeof(*a))
+
+
+void *_arr_create();
+void *_arr_reserve(void *a, size_t nmemb, size_t sz);
+void *_arr_insert_null(void *a, u32 idx, size_t sz);
+void _arr_remove(void *a, void *e, size_t sz);
+void _arr_reverse(void *a, size_t sz);
+u32 _arr_contains(void *a, const void *elem, size_t sz);
+void *_arr_upper(void *a, const void *elem,
+                 int(*comp)(const void *, const void*), size_t sz);
+
+#endif
