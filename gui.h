46dd0b2d2ff4002e3c3ec7d69f521189f9b5d8a4
rgriege <rgriege@sbcglobal.net>
2018-01-24 22:11:12 -0600

    Allow overriding default font file path

--- gui.h
+++ gui.h
@@ -250,6 +250,10 @@ typedef enum img_scale
 	IMG_CENTERED,
 } img_scale_t;
 
+#ifndef GUI_FONT_FILE_PATH
+#define GUI_FONT_FILE_PATH "Roboto.ttf"
+#endif
+
 void gui_line(gui_t *gui, s32 x0, s32 y0, s32 x1, s32 y1, s32 w, color_t c);
 void gui_linef(gui_t *gui, r32 x0, r32 y0, r32 x1, r32 y1, s32 w, color_t c);
 void gui_rect(gui_t *gui, s32 x, s32 y, s32 w, s32 h, color_t fill, color_t line);
@@ -2751,7 +2755,7 @@ font_t *gui__get_font(gui_t *gui, u32 sz)
 		return font;
 
 	font = array_append_null(gui->fonts);
-	if (font_load(font, "Roboto.ttf", sz)) {
+	if (font_load(font, GUI_FONT_FILE_PATH, sz)) {
 		return font;
 	} else {
 		array_pop(gui->fonts);
