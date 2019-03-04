3148bf500825aca45d87562a44469465b39e97ae
rgriege <rgriege@sbcglobal.net>
2019-03-04 14:20:27 -0600

    Cleanup localization scripts

--- dump_strings_default.sh
+++ dump_strings_default.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ $# -eq 0 ]; then
+echo "Usage: dump_strings <SRC_DIR> [HASH_PROGRAM]"
+exit 1
+fi
 
-mapfile -t STRINGS < <(grep -roh 'LOCALIZE\(_STATIC\)\?("[^"]\+")' $1 | cut -d \" -f 2 | sort | uniq)
+SRC=$1
+
+if [ $# -lt 2 ]; then
+HASH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/hash"
+else
+HASH=$2
+fi
+
+if [ ! -d $SRC ]; then
+echo "$SRC not found"
+exit 1
+fi
+
+if [ ! -f $HASH ]; then
+echo "$HASH not found"
+exit 1
+fi
+
+mapfile -t STRINGS < <(grep -roh 'LOCALIZE\(_STATIC\)\?("[^"]\+")' $SRC | cut -d \" -f 2 | sort | uniq)
 echo num_strings: ${#STRINGS[@]}
 for STRING in "${STRINGS[@]}"
 do
-	echo id: $($DIR/hash $STRING)
+	echo id: $($HASH $STRING)
 	echo str: $STRING
 done
