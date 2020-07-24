03654f4986647226f644f1915a22e195ad9a1aab
rgriege <rgriege@sbcglobal.net>
2020-07-24 09:13:14 -0500

    Expose value npt text buffer for custom widgets

--- gui.h
+++ gui.h
@@ -367,6 +367,7 @@ extern const gui_npt_filter_t g_gui_npt_filter_hex;
 
 b32 gui_npt_filter(gui_npt_filter_p filter, s32 codepoint);
 const char *gui_npt_val_buf(const gui_t *gui);
+char       *gui_npt_val_buf_mut(gui_t *gui);
 
 typedef struct gui_widget_bounds
 {
@@ -4268,6 +4269,11 @@ s32 gui_npt_txt_ex(gui_t *gui, s32 x, s32 y, s32 w, s32 h, char *txt, u32 n,
 }
 
 const char *gui_npt_val_buf(const gui_t *gui)
+{
+	return gui->npt.val_buf;
+}
+
+char *gui_npt_val_buf_mut(gui_t *gui)
 {
 	return gui->npt.val_buf;
 }
